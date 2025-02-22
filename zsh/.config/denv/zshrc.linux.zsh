fe8e37bf09f2863e7f43cdbd81426b701ce31388
Michael D <mikeymop@users.noreply.github.com>
2025-02-22 17:53:43 -0500

    Add home and end cursor movement shortcuts (#2)

--- zsh/.config/denv/zshrc.linux.zsh
+++ zsh/.config/denv/zshrc.linux.zsh
@@ -1,4 +1,4 @@
-# User specific environment
+# Add User specific environment to PATH
 if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
     PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 fi
@@ -12,8 +12,12 @@ eval "$(oh-my-posh init zsh --config $HOME/.config/omp/linux.omp.toml)"
 source $HOME/.config/denv/zsh-dev.zsh
 
 # Keymaps
+# from bindkey -M main
+# bindkey '^[v' .describe-key-briefly can help you print them
 bindkey "^[[1;5C" forward-word  # ctrl + ->
 bindkey "^[[1;5D" backward-word # ctrl + <-
+bindkey '^[[H' beginning-of-line # replace ctrl + a cursor start of line
+bindkey '^[[F' end-of-line # replace ctrl + e cursor end of line
 
 # Aliases
 alias wlc="wl-copy"
